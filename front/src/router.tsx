45ae2342896e8824d68bcf8c946d4ba43e928324
ErwanDecoster <wanerd2@gmail.com>
2025-02-10 16:22:15 +0100

    fix smalls router issues

--- front/src/router.tsx
+++ front/src/router.tsx
@@ -1,23 +1,18 @@
 import { createBrowserRouter, Navigate } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout.tsx';
 import { AddProtectedImages } from './views/AddProtectedImages/AddProtectedImages.tsx';
-import { Index } from './views/Index.tsx';
 import { ProtectedImages } from './views/ProtectedImages/ProtectedImages.tsx';
 
 export const router = createBrowserRouter([
   {
-    path: '/',
     element: <MainLayout />,
     children: [
       {
-        index: true,
-        element: <Index />,
-      },
-      {
         path: '*',
-        element: <Navigate to="/" />,
+        element: <Navigate to="/protected-images" />,
       },
       {
+        index: true,
         path: '/protected-images',
         element: <ProtectedImages />,
       },
