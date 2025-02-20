a47bb27b4dc1d71bc51a22f044f5315b470b9e6c
Anthony Simonet-Boulogne <anthony@iex.ec>
2025-02-20 09:57:20 +0100

    Reduced image size

--- idapp/sconify.sh
+++ idapp/sconify.sh
@@ -1,11 +1,20 @@
-IMG_NAME=image-caption-matcher-dapp
-IMG_FROM=<YOUR-DOCKER-HUB-USERNAME>/${IMG_NAME}:1.0.0
-IMG_TO=<YOUR-DOCKER-HUB-USERNAME>/${IMG_NAME}:1.0.0
-docker run -it \
+IMG_NAME=image-caption-matcher
+IMG_FROM=ansimonet/${IMG_NAME}:non-tee
+IMG_TO=ansimonet/${IMG_NAME}:tee
+
+SCONIFIER_IMAGE="registry.scontain.com/scone-production/iexec-sconify-image:5.7.6-v15"
+
+if [ $(uname -p | grep arm) ]; then
+	SCONIFIER_IMAGE="registry.scontain.com/scone-debug/iexec-sconify-image-unlocked:5.8.9"
+	PLATFORM="--platform=linux/amd64"
+	DOCKER_PLATFORM="--docker-target-platform=linux/amd64"
+fi
+
+docker run -it ${PLATFORM} \
             -v /var/run/docker.sock:/var/run/docker.sock \
-            registry.scontain.com/scone-production/iexec-sconify-image:5.7.6-v15 \
+            ${SCONIFIER_IMAGE} \
             sconify_iexec \
-            --base=ubuntu:20.04 \
+            ${DOCKER_PLATFORM} \
             --name=${IMG_NAME} \
             --from=${IMG_FROM} \
             --to=${IMG_TO} \
@@ -13,13 +22,9 @@ docker run -it \
             --fs-dir=/app \
             --host-path=/etc/hosts \
             --host-path=/etc/resolv.conf \
-            --binary=/root/miniconda/bin/python3.8 \
-            --fs-dir=/root/miniconda/lib/ \
+            --binary=/usr/local/bin/python3.8 \
             --heap=4610612736 \
             --dlopen=1 \
-            --no-color \
-            --verbose \
-            --debug \
             && echo -e "\n------------------\n" \
             && echo "successfully built TEE docker image => ${IMG_TO}" \
             && echo "application mrenclave.fingerprint is $(docker run -it --rm -e SCONE_HASH=1 ${IMG_TO})"
